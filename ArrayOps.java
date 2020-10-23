77c3080625166bcbed4a68a5aba8913ec124917e
emmachio <emmany2409@gmail.com>
2020-10-23 13:17:59 -0400

    finished sum

---
+++ ArrayOps.java
@@ -0,0 +1,20 @@
+public class ArrayOps{
+
+  public static void main(String[] args) {
+    // int[] a = {};
+    // System.out.println(sum(a));
+  }
+
+  public static int sum(int[] arr) {
+    int sum = 0;
+    for(int i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    return sum;
+  }
+
+  // public static int largest(int[] arr) {
+  //
+  // }
+
+}
--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 public class Tester {
 
   public static void main(String[] args) {
-
+    int[] a = {1,2,3};
+    System.out.println(ArrayOps.sum(a));
   }
-  
+
 }
