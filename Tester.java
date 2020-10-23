b65a660ab5c2670fe61cf00c91281bf24358a8ec
emmachio <emmany2409@gmail.com>
2020-10-23 13:24:36 -0400

    added largest

--- Tester.java
+++ Tester.java
@@ -1,8 +1,9 @@
 public class Tester {
 
   public static void main(String[] args) {
-    int[] a = {1,2,3};
+    int[] a = {1,2,10};
     System.out.println(ArrayOps.sum(a));
+    System.out.println(ArrayOps.largest(a));
   }
 
 }
