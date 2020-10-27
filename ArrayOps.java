64dcb04247db922fe647971af5d8bbca68f63631
emmachio <emmany2409@gmail.com>
2020-10-26 21:25:18 -0400

    added sumRows

--- ArrayOps.java
+++ ArrayOps.java
@@ -21,4 +21,16 @@ public class ArrayOps{
     return max;
   }
 
+  public static int[] sumRows(int[][] matrix) {
+    int[] sumMatrix = new int[matrix.length];
+    for(int i = 0; i < matrix.length; i++) {
+      int sum = 0;
+      for(int j = 0; j < matrix[i].length; j++) {
+        sum = sum + matrix[i][j];
+      }
+      sumMatrix[i] = sum;
+    }
+    return sumMatrix;
+  }
+
 }
