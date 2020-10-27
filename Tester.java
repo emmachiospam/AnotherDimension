fdb5a5e8b797c26f7efe8d0f237759690bb5dab3
emmachio <emmany2409@gmail.com>
2020-10-26 21:59:31 -0400

    updated

--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 public class Tester {
 
   public static void main(String[] args) {
-    int[][] a = {{1,2,10}, {2,2,2}, {2,3,4}};
+    int[][] a = {{1,0,12,-1}, {7,-2,2,1}, {-5,-2,2,-9}};
+    int[] b = {1,3,5};
     // System.out.println(ArrayOps.sum(a));
     // System.out.println(ArrayOps.largest(a));
     // System.out.println(arrToString(ArrayOps.sumRows(a)));
