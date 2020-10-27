0e04e7e00b38ac7ca67709744ce1178640f33c23
emmachio <emmany2409@gmail.com>
2020-10-26 21:25:37 -0400

    added sumRows

--- Tester.java
+++ Tester.java
@@ -1,9 +1,23 @@
 public class Tester {
 
   public static void main(String[] args) {
-    int[] a = {1,2,10};
-    System.out.println(ArrayOps.sum(a));
-    System.out.println(ArrayOps.largest(a));
+    int[][] a = {{1,2,10}, {2,2,2}, {2,3,4}};
+    // System.out.println(ArrayOps.sum(a));
+    // System.out.println(ArrayOps.largest(a));
+    System.out.println(arrToString(ArrayOps.sumRows(a)));
+  }
+
+  public static String arrToString(int[] arr){
+  String result = "{";
+  if(arr.length > 0) {
+    for(int i = 0; i < arr.length-1; i++) {
+      result = result + Integer.toString(arr[i]);
+      result = result + ", ";
+    }
+    result = result + Integer.toString(arr[arr.length-1]);
+  }
+  result = result + "}";
+  return result;
   }
 
 }
