bd1daa192784afedf7efa222f90aa88508e466c1
emmachio <emmany2409@gmail.com>
2020-10-26 22:44:08 -0400

    added SumCols

--- Tester.java
+++ Tester.java
@@ -1,13 +1,15 @@
 public class Tester {
 
   public static void main(String[] args) {
-    int[][] a = {{1,0,12,-1}, {7,-2,2,1}, {-5,-2,2,-9}};
+    int[][] a = { {  2,  2, 2 },
+   {  2, 2, 2 } };
     int[] b = {1,3,5};
     // System.out.println(ArrayOps.sum(a));
     // System.out.println(ArrayOps.largest(a));
     // System.out.println(arrToString(ArrayOps.sumRows(a)));
     // System.out.println(arrToString(ArrayOps.largestInRows(a)));
-    System.out.println(ArrayOps.sum(b));
+    // System.out.println(ArrayOps.sum(b));
+    // System.out.println(arrToString(ArrayOps.sumCols(a)));
   }
 
   public static String arrToString(int[] arr){
